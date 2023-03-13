e3e17d0c703962faaabf1ef653865248ef1be973
Noor Chasib <noorchasib@gmail.com>
2023-03-13 12:49:27 -0700

    create category components from backend data

--- components/categories.js
+++ components/categories.js
@@ -1,8 +1,26 @@
 import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CategoryCard from "./CategoryCard";
+import client, { urlFor } from "../sanity";
 
 const Categories = () => {
+	const [categories, setCategories] = useState([]);
+	
+
+	useEffect(() => {
+		client
+			.fetch(
+				`
+				*[_type == "category"] 
+			`
+			)
+			.then((data) => {
+				setCategories(data);
+			});
+	}, []);
+
+	console.log("categories", categories)
+
 	return (
 		<ScrollView
 			contentContainerStyle={{
@@ -13,30 +31,14 @@ const Categories = () => {
 			showsHorizontalScrollIndicator={false}
 		>
 			{/* Category Card */}
-			<CategoryCard
-				imgUrl="https://links.papareact.com/gn7"
-				title="Testing 1"
-			/>
-			<CategoryCard
-				imgUrl="https://links.papareact.com/gn7"
-				title="Testing 2"
-			/>
-			<CategoryCard
-				imgUrl="https://links.papareact.com/gn7"
-				title="Testing 3"
-			/>
-			<CategoryCard
-				imgUrl="https://links.papareact.com/gn7"
-				title="Testing 4"
-			/>
-			<CategoryCard
-				imgUrl="https://links.papareact.com/gn7"
-				title="Testing 5"
-			/>
-			<CategoryCard
-				imgUrl="https://links.papareact.com/gn7"
-				title="Testing 6"
-			/>
+
+			{categories?.map((category) => (
+				<CategoryCard
+					key={category._id}
+					imgUrl={urlFor(category.image).width(200).url()}
+					title={category.name}
+				/>
+			))}
 		</ScrollView>
 	);
 };
--- sanity.js
+++ sanity.js
@@ -11,7 +11,4 @@ const client = createClient({
 const builder = imageUrlBuilder(client);
 export const urlFor = (source) => builder.image(source);
 
-// RUN THIS to add exception for localhost 3000 CORS policy
-// sanity cors add http://localhost:3000
-
 export default client;
