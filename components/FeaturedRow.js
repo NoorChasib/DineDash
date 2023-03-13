17da6d1717effde2ef61f79d5109fb01f54a9dd3
Noor Chasib <noorchasib@gmail.com>
2023-03-13 11:34:10 -0700

    implement backend queeries for frontend passthrough and remove hardcoded values

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,17 +1,42 @@
 import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ArrowRightIcon } from "react-native-heroicons/outline";
 import RestaurantCard from "./RestaurantCard";
+import client from "../sanity";
 
 const FeaturedRow = ({ id, title, description }) => {
+	const [restaurants, setRestaurants] = useState([]);
+
+	useEffect(() => {
+		client
+			.fetch(
+				`
+			*[_type == "featured" && _id == $id] {
+				...,
+				restaurants[]->{
+					...,
+					dishes[]->,
+					type-> {
+						name
+					}
+				},
+			}[0]
+		`,
+				{ id }
+			)
+			.then((data) => {
+				setRestaurants(data?.restaurants);
+			});
+	}, []);
+
 	return (
 		<View>
-			<View className="mt-4 flex-row items-center justify-between px-4">
-				<Text className="font-bold text-lg">{title}</Text>
+			<View className="flex-row items-center justify-between px-4 mt-4">
+				<Text className="text-lg font-bold">{title}</Text>
 				<ArrowRightIcon color="#00CCBB" />
 			</View>
 
-			<Text className="text-xs text-gray-500 px-4">{description}</Text>
+			<Text className="px-4 text-xs text-gray-500">{description}</Text>
 
 			<ScrollView
 				horizontal
@@ -22,54 +47,22 @@ const FeaturedRow = ({ id, title, description }) => {
 				className="pt-4"
 			>
 				{/* Restaurant Card */}
-				<RestaurantCard
-					id={123}
-					imgUrl="https://links.papareact.com/gn7"
-					title="Yo! Sushi"
-					rating={4.5}
-					genre="Japanese"
-					address="123 Main St"
-					short_description="This is a short description"
-					dishes={[]}
-					long={20}
-					lat={0}
-				/>
-				<RestaurantCard
-					id={123}
-					imgUrl="https://links.papareact.com/gn7"
-					title="Yo! Sushi"
-					rating={4.5}
-					genre="Japanese"
-					address="123 Main St"
-					short_description="This is a short description"
-					dishes={[]}
-					long={20}
-					lat={0}
-				/>
-				<RestaurantCard
-					id={123}
-					imgUrl="https://links.papareact.com/gn7"
-					title="Yo! Sushi"
-					rating={4.5}
-					genre="Japanese"
-					address="123 Main St"
-					short_description="This is a short description"
-					dishes={[]}
-					long={20}
-					lat={0}
-				/>
-				<RestaurantCard
-					id={123}
-					imgUrl="https://links.papareact.com/gn7"
-					title="Yo! Sushi"
-					rating={4.5}
-					genre="Japanese"
-					address="123 Main St"
-					short_description="This is a short description"
-					dishes={[]}
-					long={20}
-					lat={0}
-				/>
+
+				{restaurants?.map((restaurant) => (
+					<RestaurantCard
+						key={restaurant._id}
+						id={restaurant._id}
+						imgUrl={restaurant.image}
+						address={restaurant.address}
+						title={restaurant.name}
+						dishes={restaurant.dishes}
+						rating={restaurant.rating}
+						short_description={restaurant.short_description}
+						genre={restaurant.type?.name}
+						long={restaurant.long}
+						lat={restaurant.lat}
+					/>
+				))}
 			</ScrollView>
 		</View>
 	);
--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -2,6 +2,7 @@ import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { StarIcon } from "react-native-heroicons/solid";
 import { MapPinIcon } from "react-native-heroicons/outline";
+import { urlFor } from "../sanity";
 
 const RestaurantCard = ({
 	id,
@@ -16,15 +17,15 @@ const RestaurantCard = ({
 	lat,
 }) => {
 	return (
-		<TouchableOpacity className="bg-white mr-3 shadow">
+		<TouchableOpacity className="mr-3 bg-white shadow">
 			<Image
 				source={{
-					uri: imgUrl,
+					uri: urlFor(imgUrl).url(),
 				}}
-				className="h-36 w-64 rounded-sm"
+				className="w-64 rounded-sm h-36"
 			/>
 			<View className="px-3 pb-4">
-				<Text className="font-bold text-lg pt-2">{title}</Text>
+				<Text className="pt-2 text-lg font-bold">{title}</Text>
 				<View className="flex-row items-center space-x-1">
 					<StarIcon color="green" opacity={0.5} size={22} />
 					<Text className="text-xs text-gray-500">
