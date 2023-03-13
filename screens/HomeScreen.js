99648a041c4e20c2d8af50967e17989b62377520
Noor Chasib <noorchasib@gmail.com>
2023-03-12 23:43:31 -0700

    fix for url ios error

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -46,22 +46,20 @@ const HomeScreen = () => {
 			});
 	}, []);
 
-	console.log("featuredCategories:", featuredCategories);
-
 	return (
-		<SafeAreaView className="bg-white pt-5">
+		<SafeAreaView className="pt-5 bg-white">
 			{/* Header */}
-			<View className="flex-row pb-3 items-center mx-4 space-x-2">
+			<View className="flex-row items-center pb-3 mx-4 space-x-2">
 				<Image
 					source={{
 						uri: "https://links.papareact.com/wru",
 					}}
-					className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+					className="p-4 bg-gray-300 rounded-full h-7 w-7"
 				/>
 
 				<View className="flex-1">
-					<Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-					<Text className="font-bold text-xl">
+					<Text className="text-xs font-bold text-gray-400">Deliver Now!</Text>
+					<Text className="text-xl font-bold">
 						Current Location
 						<ChevronDownIcon size={20} color="#00CCBB" />
 					</Text>
@@ -70,8 +68,8 @@ const HomeScreen = () => {
 			</View>
 
 			{/* Search */}
-			<View className="flex-row items-center space-x-2 pb-2 mx-4">
-				<View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+			<View className="flex-row items-center pb-2 mx-4 space-x-2">
+				<View className="flex-row flex-1 p-3 space-x-2 bg-gray-200">
 					<MagnifyingGlassIcon color="gray" size={20} />
 					<TextInput
 						placeholder="Restaurants and cuisines"
