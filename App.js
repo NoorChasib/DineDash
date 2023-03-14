6e28423a4376ce3b6b7ae11d773324879fb29605
Noor Chasib <noorchasib@gmail.com>
2023-03-14 15:46:44 -0700

    create delivery screen dependencies

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
 import BasketScreen from "./screens/BasketScreen";
 import PreparingOrderScreen from "./screens/PreparingOrderScreen";
+import DeliveryScreen from "./screens/DeliveryScreen";
 import "react-native-url-polyfill/auto";
 import { Provider } from "react-redux";
 import { store } from "./store";
@@ -27,6 +28,11 @@ export default function App() {
 						<Stack.Screen
 							name="PreparingOrder"
 							component={PreparingOrderScreen}
+							options={{ presentation: "fullScreenModal", headerShown: false }}
+						/>
+						<Stack.Screen
+							name="Delivery"
+							component={DeliveryScreen}
 							options={{ presentation: "fullScreenModal", headerShown: false }}
 						/>
 					</Stack.Navigator>
